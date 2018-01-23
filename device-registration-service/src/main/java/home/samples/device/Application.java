46290477824f2462277511472c5cce2cf2e37200
akaczmarek <adamw.kaczmarek@gmail.com>
2018-01-23 23:37:01 +0100

    Adding authorization (JWT).

--- device-registration-service/src/main/java/home/samples/device/Application.java
+++ device-registration-service/src/main/java/home/samples/device/Application.java
@@ -1,18 +1,20 @@
 package home.samples.device;
 
-import home.samples.context.UserContext;
+
 import home.samples.context.UserContextFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-import javax.servlet.Filter;
+
 
 @SpringBootApplication
 @EnableEurekaClient
 @Configuration
+@EnableResourceServer
 public class Application {
 
     public static void main(String[] args) {
