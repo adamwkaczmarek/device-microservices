54a1e7f5c04a341695fc70fc95263abb4246c764
akaczmarek <adam.kaczmarek@onwelo.com>
2018-03-05 12:15:27 +0100

    Travis configuration update.

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region eu-central-1 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster devices-ms
+ecs-cli configure profile --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+ecs-cli configure --cluster devices-ms  --region eu-central-1
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
